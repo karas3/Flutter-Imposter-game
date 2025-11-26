3f7e30ef2a36132da64d04163fedccff86fd375d
dawid <dawidmakarewicz74@gmail.com>
2025-11-26 23:55:31 +0100

    Changed layout and modified navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,14 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String animal = "Click + to generate random animal";
+  var selectedIndex = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp( 
       theme: ThemeData(useMaterial3: false, scaffoldBackgroundColor: const Color.fromARGB(255, 228, 228, 228)),   //page backgroundcolor
+
+      
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 50, 125, 175),   //top bar color
@@ -28,6 +31,8 @@ class _MyAppState extends State<MyApp> {
           title: const Text("Imposter Game"),   // text on top bar
           elevation: 20,  //Set shadow on top bar
         ),
+
+
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
@@ -37,6 +42,7 @@ class _MyAppState extends State<MyApp> {
           },
         ),
 
+
         body: ListView(
           scrollDirection: Axis.vertical,
           children: [
@@ -45,16 +51,25 @@ class _MyAppState extends State<MyApp> {
               children: [
                 Column(
                   children: [
+                    Container(
+                      alignment: Alignment.center,    // text aligment
+                      margin: EdgeInsets.all(10),
+                      child: const Text(
+                            "Create Lobby",
+                            style: TextStyle(height: 1, fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+
+
+
                     Stack(
                       children: [
                         Container(
-                          alignment: Alignment.center,
                           margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          height: 300,
-                          width: 300,
+                          height: 100,
+                          width: 350,
                           decoration: BoxDecoration(
-                            color: Colors.lightBlue,
+                            color: const Color.fromARGB(255, 167, 167, 167),
                             boxShadow: [
                               BoxShadow(
                                 color: const Color.fromARGB(255, 0, 0, 0),
@@ -64,12 +79,34 @@ class _MyAppState extends State<MyApp> {
                               )
                             ],
                           ),
-                          child: const Text(
-                            "Hello World!",
-                            style: TextStyle(height: 1, fontSize: 48),
+                          child: Row(
+                            children: [
+                              Container(    // small color stripe
+                                margin: EdgeInsets.only(left: 20.0),
+                                height: 100,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(255, 0, 110, 255),
+                                ),
+                              ),
+                              Container(    // Container for input field
+                                alignment: Alignment.center,
+                                margin: EdgeInsets.only(left: 20),
+                                width: 240,
+                                height: 100,
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: "Input player 1",
+                                    border: UnderlineInputBorder(),
+                                  ),
+                                  style: TextStyle(
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Positioned(top: 20,  left: 280, child: Icon(Icons.verified)),
                       ],
                     ),
             
@@ -104,18 +141,24 @@ class _MyAppState extends State<MyApp> {
 
 
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "home",
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
-              label: "menu"
+              icon: Icon(Icons.play_arrow),
+              label: "play",
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: "settings"
+              icon: Icon(Icons.info),
+              label: "informations"
             ),
           ]
         ),
---
+++ lib/src/lobby.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String _name = "";
+  var _color = Color.fromARGB(0, 0, 0, 0);
+  var _role = "";
+}
