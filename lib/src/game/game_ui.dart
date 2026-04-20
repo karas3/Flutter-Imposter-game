9a180b369707bf32b662a873f9884100db15d459
dawid <dawidmakarewicz74@gmail.com>
2026-04-20 21:50:23 +0200

    Made hidding box background a gradient

--- lib/src/game/game_ui.dart
+++ lib/src/game/game_ui.dart
@@ -6,14 +6,17 @@ import 'game_object.dart';
 import 'package:imposter_party_game/src/ui_elements/custom_text.dart';
 
 class TextBox extends StatefulWidget {
-  TextBox({super.key});
+  const TextBox({super.key});
 
   @override
   State<TextBox> createState() => _TextBoxState();
 }
 
-class _TextBoxState extends State<TextBox> {
-  Offset containerOffset = Offset.zero;
+class _TextBoxState extends State<TextBox> with SingleTickerProviderStateMixin {
+  Offset _containerOffset = Offset.zero;
+  late final AnimationController _animationController;
+  List<Tween<double>> _randomGradientAligment = [Tween(begin: 0.0, end: 1.0), Tween(begin: 0.0, end: 1.0)];
+
   
   @override
   Widget build(BuildContext context) {
@@ -38,32 +41,71 @@ class _TextBoxState extends State<TextBox> {
         ),
         GestureDetector(
           onPanUpdate: (details) {
-            if(containerOffset.dy + details.delta.dy > 0) { //prevents containerOffset becoming negative
+            if(_containerOffset.dy + details.delta.dy > 0) { //prevents _containerOffset becoming negative
               double smoothness = 0.1;  // smaller the value more smooth the ending
               double maxOffset = 160;
-              setState(() => containerOffset = containerOffset + details.delta / (1 + pow((e), smoothness * (containerOffset.dy - maxOffset)).toDouble())); // sigmoid function
+              setState(() => _containerOffset = _containerOffset + details.delta / sigmoid(smoothness, maxOffset));
             }
           },
           onPanEnd: (details) {
-            setState(() => containerOffset = Offset.zero);
+            setState(() => _containerOffset = Offset.zero);
           },
-          child: Container(
-            width: 300,
-            height: 200,
-            transform: Matrix4.translationValues(0, containerOffset.dy, 0),
-            alignment: Alignment(0, containerOffset.dy),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.inversePrimary,
-              borderRadius: BorderRadius.all(Radius.circular(30)),
-            ),
-            child: Icon(
-              Icons.arrow_downward_rounded,
-              size: 200,
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) => Container(
+              width: 300,
+              height: 200,
+              transform: Matrix4.translationValues(0, _containerOffset.dy, 0),
+              alignment: Alignment(0, _containerOffset.dy),
+              decoration: BoxDecoration(
+                // color: Theme.of(context).colorScheme.inversePrimary,
+                gradient: RadialGradient(
+                  colors: [const Color.fromARGB(255, 93, 118, 165), const Color.fromARGB(255, 39, 14, 97)],
+                  center: AlignmentGeometry.xy(_randomGradientAligment[0].evaluate(_animationController), _randomGradientAligment[1].evaluate(_animationController)),
+                  radius: 0.8
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              child: Icon(
+                Icons.arrow_downward_rounded,
+                size: 200,
+              ),
             ),
           ),
         )
       ],
     );
+  }
+
+  double sigmoid(double smoothness, double maxOffset) {
+    return 1 + pow((e), smoothness * (_containerOffset.dy - maxOffset)).toDouble();
+  }
+
+  void _animationLoop() {
+    _animationController.forward().then((_) {
+      _animationController.reset();
+      setState(() {
+          _randomGradientAligment = [Tween(begin: _randomGradientAligment[0].end, end: Random().nextDouble() - 0.5), Tween(begin: _randomGradientAligment[1].end, end: Random().nextDouble() - 0.5)];
+      });
+
+      _animationLoop();
+    });
+  }
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animationLoop();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
 
